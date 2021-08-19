955683e1d827175dd3cf1a8e55a8b9c494c999fe
colby <colbykrawczyk10@gmail.com>
2021-08-19 04:08:42 -0400

    Created Party and PartyManager

---
+++ src/main/java/net/elytrapvp/elytraduels/party/Party.java
@@ -0,0 +1,68 @@
+package net.elytrapvp.elytraduels.party;
+
+import org.bukkit.entity.Player;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Represents a group of players
+ * outside of a Game.
+ */
+public class Party {
+    private final PartyManager partyManager;
+    private Player leader;
+    private final Set<Player> members = new HashSet<>();
+
+    /**
+     * Create a new party.
+     * @param partyManager PartyManager used.
+     * @param leader Leader of the party.
+     */
+    public Party(PartyManager partyManager, Player leader) {
+        this.partyManager = partyManager;
+        this.leader = leader;
+    }
+
+    /**
+     * Disband the party.
+     */
+    public void disband() {
+        // TODO: Properly handle players when disbanded.
+        partyManager.disbandParty(this);
+    }
+
+    /**
+     * Get the current leader of the party.
+     * @return Current party leader.
+     */
+    public Player getLeader() {
+        return leader;
+    }
+
+    /**
+     * Get all party members.
+     * @return Members of the party.
+     */
+    public Set<Player> getMembers() {
+        return members;
+    }
+
+    /**
+     * Get all players in the party.
+     * @return All players.
+     */
+    public Set<Player> getPlayers() {
+        Set<Player> players = new HashSet<>(getMembers());
+        players.add(getLeader());
+        return players;
+    }
+
+    /**
+     * Change the leader of the party.
+     * @param leader New party leader.
+     */
+    public void setLeader(Player leader) {
+        this.leader = leader;
+    }
+}
--- src/main/java/net/elytrapvp/elytraduels/party/PartyManager.java
+++ src/main/java/net/elytrapvp/elytraduels/party/PartyManager.java
@@ -1,4 +1,56 @@
 package net.elytrapvp.elytraduels.party;
 
+import org.bukkit.entity.Player;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Manages all existing party.
+ */
 public class PartyManager {
+    private final Set<Party> parties = new HashSet<>();
+
+    /**
+     * Create a new party.
+     * @param leader Leader of the party,
+     * @return Party that was created.
+     */
+    public Party createParty(Player leader) {
+        Party party = new Party(this, leader);
+        getParties().add(party);
+        return party;
+    }
+
+    /**
+     * Disbands an active party.
+     * @param party Party to disband.
+     */
+    public void disbandParty(Party party) {
+        getParties().remove(party);
+    }
+
+    /**
+     * Get the party a player is in.
+     * Returns null if not in a party.
+     * @param player Player to get party of.
+     * @return Party the player is in.
+     */
+    public Party getParty(Player player) {
+        for(Party party : getParties()) {
+            if(party.getMembers().contains(player)) {
+                return party;
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * Get all current parties.
+     * @return All current paries.
+     */
+    public Set<Party> getParties() {
+        return parties;
+    }
 }
