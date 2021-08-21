f2830cc8b34352695e1b2fd4496901c3f9c4d8b0
colby <colbykrawczyk10@gmail.com>
2021-08-21 17:52:49 -0400

    Added timer object

---
+++ src/main/java/net/elytrapvp/elytraduels/utils/Timer.java
@@ -0,0 +1,79 @@
+package net.elytrapvp.elytraduels.utils;
+
+import net.elytrapvp.elytraduels.ElytraDuels;
+import org.bukkit.scheduler.BukkitRunnable;
+
+/**
+ * Creates a timer that counts up.
+ * Used in queue and in match.
+ */
+public class Timer {
+    private final ElytraDuels plugin;
+    private int seconds;
+    private int minutes;
+    private BukkitRunnable task;
+
+    /**
+     * Create a timer.
+     */
+    public Timer(ElytraDuels plugin) {
+        this.plugin = plugin;
+        seconds = 0;
+        minutes = 0;
+
+        task = new BukkitRunnable() {
+            @Override
+            public void run() {
+                seconds++;
+
+                if(seconds == 60) {
+                    seconds = 0;
+                    minutes ++;
+                }
+            }
+        };
+    }
+
+    /**
+     * Reset the timer.
+     */
+    public void reset() {
+        seconds = 0;
+        minutes = 0;
+    }
+
+    /**
+     * Start the timer.
+     */
+    public void start() {
+        task.runTaskTimer(plugin, 0, 20);
+    }
+
+    /**
+     * Stop the timer.
+     */
+    public void stop() {
+        task.cancel();
+    }
+
+    /**
+     * Converts the timer into a String
+     * @return String version of timer.
+     */
+    public String toString() {
+        String minute = "";
+        if(minutes < 10) {
+            minute += "0";
+        }
+        minute += "" + minutes;
+
+        String second = "";
+
+        if(seconds < 10) {
+            second += "0";
+        }
+        second += "" + seconds;
+
+        return minute + ":" + second;
+    }
+}
