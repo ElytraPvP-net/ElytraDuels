26cb3f99cf5ecaf1b917013f55f7e5fa4fc5df5a
colby <colbykrawczyk10@gmail.com>
2021-08-21 04:33:32 -0400

    Added the configuration foundations

--- src/main/java/net/elytrapvp/elytraduels/SettingsManager.java
+++ src/main/java/net/elytrapvp/elytraduels/SettingsManager.java
@@ -1,21 +1,50 @@
 package net.elytrapvp.elytraduels;
 
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+
+/**
+ * Allows easy access to plugin configuration
+ * files. Stores spawn and arena locations.
+ */
 public class SettingsManager {
     private final ElytraDuels plugin;
 
+    private FileConfiguration config;
+    private File configFile;
+    private FileConfiguration maps;
+    private File mapsFile;
+
     public SettingsManager(ElytraDuels plugin) {
         this.plugin = plugin;
+
+        config = plugin.getConfig();
+        config.options().copyDefaults(true);
+        configFile = new File(plugin.getDataFolder(), "config.yml");
+        plugin.saveConfig();
+
+        mapsFile = new File(plugin.getDataFolder(), "maps.yml");
+        maps = YamlConfiguration.loadConfiguration(mapsFile);
+
+        if(!mapsFile.exists())
+            plugin.saveResource("maps.yml", false);
     }
 
-    public void getConfig() {
-
+    /**
+     * Get the main configuration file.
+     * @return Main configuration file.
+     */
+    public FileConfiguration getConfig() {
+        return config;
     }
 
-    public void getKits() {
-
-    }
-
-    public void getMaps() {
-
+    /**
+     * Get the maps configuration file.
+     * @return Maps configuration file.
+     */
+    public FileConfiguration getMaps() {
+        return maps;
     }
 }
