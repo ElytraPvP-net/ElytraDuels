49787fd0804bf4d1dd29b315a93cad76e89de339
colby <colbykrawczyk10@gmail.com>
2021-08-21 22:38:43 -0400

    Prevent items from being dropped

--- src/main/java/net/elytrapvp/elytraduels/ElytraDuels.java
+++ src/main/java/net/elytrapvp/elytraduels/ElytraDuels.java
@@ -32,6 +32,7 @@ public final class ElytraDuels extends JavaPlugin {
         Bukkit.getPluginManager().registerEvents(new EntityRegainhealthListener(this), this);
         Bukkit.getPluginManager().registerEvents(new EntityShootBowListener(), this);
         Bukkit.getPluginManager().registerEvents(new FoodLevelChangeListener(this), this);
+        Bukkit.getPluginManager().registerEvents(new PlayerDropItemListener(), this);
         Bukkit.getPluginManager().registerEvents(new PlayerJoinListener(this), this);
         Bukkit.getPluginManager().registerEvents(new PlayerTeleportListener(), this);
         Bukkit.getPluginManager().registerEvents(new PlayerToggleFlightListener(this), this);
