85e494728c958891a27f67ec3eab5e44718a9233
colby <colbykrawczyk10@gmail.com>
2021-11-17 22:15:13 -0500

    Fix issue with liquids

--- src/main/java/net/elytrapvp/elytraduels/listeners/BlockFromToListener.java
+++ src/main/java/net/elytrapvp/elytraduels/listeners/BlockFromToListener.java
@@ -3,6 +3,8 @@ package net.elytrapvp.elytraduels.listeners;
 import net.elytrapvp.elytraduels.ElytraDuels;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockFromToEvent;
@@ -17,12 +19,59 @@ public class BlockFromToListener implements Listener {
     @EventHandler
     public void onEvent(BlockFromToEvent event) {
         if(event.getToBlock().getType() == Material.STATIONARY_LAVA || event.getToBlock().getType() == Material.OBSIDIAN) {
-            Bukkit.getScheduler().runTaskLater(plugin, () -> event.getBlock().setType(Material.AIR), 200);
+            //Bukkit.getScheduler().runTaskLater(plugin, () -> event.getBlock().setType(Material.AIR), 200);
         }
 
-        if(event.getBlock().isLiquid()) {
+        /*
+        if(!event.getToBlock().isLiquid() && event.getToBlock().getType() != Material.AIR) {
             Bukkit.getScheduler().runTaskLater(plugin, () -> event.getToBlock().setType(Material.AIR), 200);
             return;
         }
+
+         */
+
+        /*
+        if(event.getToBlock().getType() == Material.COBBLESTONE || event.getToBlock().getType() == Material.STONE) {
+            Bukkit.getScheduler().runTaskLater(plugin, () -> event.getToBlock().setType(Material.AIR), 200);
+            return;
+        }
+
+         */
+
+
+        Material type = event.getBlock().getType();
+        if (type == Material.WATER || type == Material.STATIONARY_WATER || type == Material.LAVA || type == Material.STATIONARY_LAVA){
+            Block b = event.getToBlock();
+            if (b.getType() == Material.AIR){
+                generatesCobble(type, b);
+            }
+        }
+
+
+    }
+
+    private final BlockFace[] faces = new BlockFace[]{
+            BlockFace.SELF,
+            BlockFace.UP,
+            BlockFace.DOWN,
+            BlockFace.NORTH,
+            BlockFace.EAST,
+            BlockFace.SOUTH,
+            BlockFace.WEST
+    };
+
+    public boolean generatesCobble(Material type, Block b) {
+        Material mirrorID1 = (type == Material.WATER || type == Material.STATIONARY_WATER ? Material.LAVA : Material.WATER);
+        Material mirrorID2 = (type == Material.WATER || type == Material.STATIONARY_WATER ? Material.STATIONARY_LAVA : Material.STATIONARY_WATER);
+        for (BlockFace face : faces) {
+            Block r = b.getRelative(face, 1);
+            if (r.getType() == mirrorID1 || r.getType() == mirrorID2) {
+                Bukkit.getScheduler().runTaskLater(plugin, () -> {
+                    r.setType(Material.AIR);
+                }, 200);
+                return true;
+            }
+        }
+        return false;
     }
 }
