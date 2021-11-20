6b06816940800671b7c8aeb94763d162985a36c7
colby <colbykrawczyk10@gmail.com>
2021-11-20 15:15:24 -0500

    More changes to repulsors

--- src/main/java/net/elytrapvp/elytraduels/listeners/PlayerToggleSneakListener.java
+++ src/main/java/net/elytrapvp/elytraduels/listeners/PlayerToggleSneakListener.java
@@ -60,8 +60,8 @@ public class PlayerToggleSneakListener implements Listener {
             //Vector direction = entity.getLocation().toVector().subtract(player.getLocation().toVector()).normalize().setY(0.8);
             //Vector direction = entity.getLocation().toVector().subtract(player.getLocation().toVector()).normalize().multiply(0.5).setY(y);
             Location location = player.getLocation();
-            location.setY(player.getLocation().getY() - 1.5);
-            Vector direction = entity.getLocation().toVector().subtract(location.toVector()).normalize().multiply(new Vector(1, 1.15, 1));
+            location.setY(player.getLocation().getY() - 2);
+            Vector direction = entity.getLocation().toVector().subtract(location.toVector()).normalize().multiply(new Vector(0.9, 1.10, 0.9));
             entity.setVelocity(direction);
         }
 
