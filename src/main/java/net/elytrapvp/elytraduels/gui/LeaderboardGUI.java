08016a22149df8277c3eede5e161edd67f75eef3
colby <colbykrawczyk10@gmail.com>
2021-11-23 22:16:46 -0500

    Add wins and win streak leaderboards

--- src/main/java/net/elytrapvp/elytraduels/gui/LeaderboardGUI.java
+++ src/main/java/net/elytrapvp/elytraduels/gui/LeaderboardGUI.java
@@ -1,7 +1,6 @@
 package net.elytrapvp.elytraduels.gui;
 
 import net.elytrapvp.elytraduels.ElytraDuels;
-import net.elytrapvp.elytraduels.game.GameType;
 import net.elytrapvp.elytraduels.game.kit.Kit;
 import net.elytrapvp.elytraduels.utils.gui.CustomGUI;
 import net.elytrapvp.elytraduels.utils.item.ItemBuilder;
@@ -14,12 +13,35 @@ public class LeaderboardGUI extends CustomGUI {
     private final ElytraDuels plugin;
 
     public LeaderboardGUI(ElytraDuels plugin) {
-        super(27, "ELO Leaderboard");
+        super(27, "Leaderboards");
+        this.plugin = plugin;
+
+        ItemBuilder elo = new ItemBuilder(Material.EMERALD)
+                .setDisplayName("&a&lELO Leaderboards");
+        setItem(10, elo.build(), (p,a) -> {
+            new LeaderboardGUI(plugin, "elo").open(p);
+        });
+
+        ItemBuilder wins = new ItemBuilder(Material.IRON_SWORD)
+                .setDisplayName("&a&lWins Leaderboards");
+        setItem(13, wins.build(), (p,a) -> {
+            new LeaderboardGUI(plugin, "wins").open(p);
+        });
+
+        ItemBuilder winstreak = new ItemBuilder(Material.PAPER)
+                .setDisplayName("&a&lWin Streak Leaderboards");
+        setItem(16, winstreak.build(), (p,a) -> {
+            new LeaderboardGUI(plugin, "bestWinStreak").open(p);
+        });
+    }
+
+    public LeaderboardGUI(ElytraDuels plugin, String type) {
+        super(27, "Leaderboards");
         this.plugin = plugin;
 
         ItemBuilder global = new ItemBuilder(Material.NETHER_STAR)
                 .setDisplayName("&aGlobal");
-        addLore(global, "global", "elo");
+        addLore(global, "global", type);
         setItem(4, global.build());
 
         int i = 9;
@@ -28,7 +50,7 @@ public class LeaderboardGUI extends CustomGUI {
             ItemBuilder item = new ItemBuilder(kit.getIconMaterial(), 1)
                     .setDisplayName("&a" + kit.getName())
                     .addFlag(ItemFlag.HIDE_ATTRIBUTES);
-            addLore(item, kit.getName().toLowerCase(), "elo");
+            addLore(item, kit.getName().toLowerCase(), type);
 
             setItem(i, item.build());
             i++;
