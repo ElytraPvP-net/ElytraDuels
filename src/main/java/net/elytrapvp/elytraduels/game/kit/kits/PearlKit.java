77111e407fb63908401fede169f0d66baece0756
colby <colbykrawczyk10@gmail.com>
2021-11-29 14:44:15 -0500

    Add temporary blocks to block sumo

--- src/main/java/net/elytrapvp/elytraduels/game/kit/kits/PearlKit.java
+++ src/main/java/net/elytrapvp/elytraduels/game/kit/kits/PearlKit.java
@@ -3,16 +3,23 @@ package net.elytrapvp.elytraduels.game.kit.kits;
 import net.elytrapvp.elytraduels.ElytraDuels;
 import net.elytrapvp.elytraduels.game.kit.Kit;
 import net.elytrapvp.elytraduels.utils.item.ItemBuilder;
+import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
+import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.inventory.ItemStack;
 
 public class PearlKit extends Kit {
+    private final ElytraDuels plugin;
+
     public PearlKit(ElytraDuels plugin) {
         super(plugin, "Pearl");
+        this.plugin = plugin;
 
         setDoDamage(false);
         setIconMaterial(Material.ENDER_PEARL);
+        setGameMode(GameMode.SURVIVAL);
 
         ItemStack helmet = new ItemBuilder(Material.LEATHER_HELMET)
                 .setUnbreakable(true)
@@ -39,7 +46,15 @@ public class PearlKit extends Kit {
 
         ItemStack pearl = new ItemBuilder(Material.ENDER_PEARL, 8).build();
 
+        ItemStack wool = new ItemBuilder(Material.WOOL, 16).build();
+
         addItem(0, stick);
         addItem(1, pearl);
+        addItem(2, wool);
+    }
+
+    @Override
+    public void onBlockPlace(BlockPlaceEvent event) {
+        Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, () -> event.getBlock().setType(Material.AIR), 100);
     }
 }
