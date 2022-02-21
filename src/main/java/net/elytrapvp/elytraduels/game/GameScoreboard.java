9a35ce3e754678ebcc4584c5bb6c4c210cb06a86
colby <colbykrawczyk10@gmail.com>
2022-02-21 18:48:45 -0500

    Add max amount of abilities to the game scoreboard

--- src/main/java/net/elytrapvp/elytraduels/game/GameScoreboard.java
+++ src/main/java/net/elytrapvp/elytraduels/game/GameScoreboard.java
@@ -38,10 +38,10 @@ public class GameScoreboard extends CustomScoreboard {
             helper.setSlot(9, "  &f" + game.getKit().getName());
             helper.setSlot(8, " ");
             helper.setSlot(7, "&aAbilities");
-            helper.setSlot(6, "  &aDouble Jump: &f" + game.getDoubleJumps(player));
-            helper.setSlot(5, "  &aTriple Shot: &f" + game.getTripleShots(player));
+            helper.setSlot(6, "  &aDouble Jump: &f" + game.getDoubleJumps(player) + "/" + game.getKit().getDoubleJumps());
+            helper.setSlot(5, "  &aTriple Shot: &f" + game.getTripleShots(player) + "/" + game.getKit().getTripleShots());
             //helper.setSlot(4, "  &aRepulsor: &f" + game.getRepulsors(player));
-            helper.setSlot(4, "  &aRepulsor: &c0" + game.getRepulsors(player));
+            helper.setSlot(4, "  &aRepulsor: &c0&f/&c0");
             helper.setSlot(3, "");
             helper.setSlot(2, "&7&m------------------");
             helper.setSlot(1, "&aplay.elytrapvp.net");
