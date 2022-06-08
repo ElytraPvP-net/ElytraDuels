928494644b0587899a5fb21fa07cd25a7ba4aeb4
colby <colbykrawczyk10@gmail.com>
2022-06-08 14:02:36 -0400

    Add leather armor to bow fight kits

--- src/main/java/net/elytrapvp/elytraduels/game/kit/kits/BlockSumoKit.java
+++ src/main/java/net/elytrapvp/elytraduels/game/kit/kits/BlockSumoKit.java
@@ -11,8 +11,10 @@ import org.bukkit.GameMode;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockState;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.material.Wool;
 
 import java.util.Random;
@@ -24,6 +26,24 @@ public class BlockSumoKit extends Kit {
         setIconMaterial(Material.WOOL);
         setDoDamage(false);
         setGameMode(GameMode.SURVIVAL);
+
+        ItemStack helmet = new ItemBuilder(Material.LEATHER_HELMET)
+                .setUnbreakable(true)
+                .build();
+        ItemStack chestplate = new ItemBuilder(Material.LEATHER_CHESTPLATE)
+                .setUnbreakable(true)
+                .build();
+        ItemStack leggings = new ItemBuilder(Material.LEATHER_LEGGINGS)
+                .setUnbreakable(true)
+                .build();
+        ItemStack boots = new ItemBuilder(Material.LEATHER_BOOTS)
+                .setUnbreakable(true)
+                .build();
+
+        addItem(39, helmet);
+        addItem(38, chestplate);
+        addItem(37, leggings);
+        addItem(36, boots);
 
         addItem(0,new ItemBuilder(Material.WOOL, 64).build());
         addItem(1, new ItemBuilder(Material.SHEARS).setUnbreakable(true).build());
