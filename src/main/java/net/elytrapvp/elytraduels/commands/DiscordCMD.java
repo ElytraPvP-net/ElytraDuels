c2d2c4bc418089757135bedca2e36507ca7ff3aa
thewaydev <lukenathanaelolson@gmail.com>
2022-07-08 14:21:19 -0700

    Create DiscordCMD.java

---
+++ src/main/java/net/elytrapvp/elytraduels/commands/DiscordCMD.java
@@ -0,0 +1,26 @@
+package net.elytrapvp.elytraduels.commands;
+
+import net.elytrapvp.elytraduels.ElytraDuels;
+import net.elytrapvp.elytraduels.gui.SettingsGUI;
+import net.elytrapvp.elytraduels.utils.chat.ChatUtils;
+import org.bukkit.entity.Player;
+import org.bukkit.Bukkit;
+import org.bukkit.command.CommandSender;
+
+public class DiscordCMD extends AbstractCommand {
+    private ElytraDuels plugin;
+
+    public DiscordCMD(ElytraDuels plugin) {
+        super("discord", "", false);
+        this.plugin = plugin;
+    }
+
+    @Override
+    public void execute(CommandSender sender, String[] args) {
+        Player player = (Player) sender;
+        ChatUtils.chat(player, "https://discord.gg/jy28s9cdrA");
+        return;
+    }
+
+
+}
