86d58172c6b96196992d5abc6e40dac39da4a73d
colby <colbykrawczyk10@gmail.com>
2022-08-03 01:46:22 -0400

    Adds per-game chat

--- src/main/java/net/elytrapvp/elytraduels/listeners/PlayerChatListener.java
+++ src/main/java/net/elytrapvp/elytraduels/listeners/PlayerChatListener.java
@@ -1,6 +1,8 @@
 package net.elytrapvp.elytraduels.listeners;
 
 import net.elytrapvp.elytraduels.ElytraDuels;
+import net.elytrapvp.elytraduels.game.Game;
+import net.elytrapvp.elytraduels.game.team.TeamColor;
 import net.elytrapvp.elytraduels.party.Party;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -16,6 +18,11 @@ public class PlayerChatListener implements Listener {
 
     @EventHandler
     public void onChat(AsyncPlayerChatEvent event) {
+        partyChat(event);
+        gameChat(event);
+    }
+
+    public void partyChat(AsyncPlayerChatEvent event) {
         Player player = event.getPlayer();
         Party party = plugin.getPartyManager().getParty(player);
 
@@ -29,5 +36,28 @@ public class PlayerChatListener implements Listener {
 
         event.setCancelled(true);
         party.sendMessage("&a&lParty &8» &f" + player.getName() + "&8: &a" + event.getMessage());
+    }
+
+    public void gameChat(AsyncPlayerChatEvent event) {
+        if(event.isCancelled()) {
+            return;
+        }
+
+        Player player = event.getPlayer();
+        Game game = plugin.getGameManager().getGame(player);
+
+        if(game == null) {
+            return;
+        }
+
+        event.setCancelled(true);
+
+        if(game.getSpectators().contains(player)) {
+            game.broadcast("&7[SPECTATOR] " + player.getName() + " &8» &7" + event.getMessage());
+            return;
+        }
+
+        TeamColor teamColor = game.getTeam(player).getTeamColor();
+        game.broadcast(teamColor.getChatColor() + "[" + teamColor + "] &7" + player.getName() + " &8» &7" + event.getMessage());
     }
 }
