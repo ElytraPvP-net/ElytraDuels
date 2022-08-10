a2a708a6e3616daf7f2d22b867853750a185569f
colby <colbykrawczyk10@gmail.com>
2022-08-10 16:04:36 -0400

    Adds more team colors

--- src/main/java/net/elytrapvp/elytraduels/game/team/TeamColor.java
+++ src/main/java/net/elytrapvp/elytraduels/game/team/TeamColor.java
@@ -8,6 +8,7 @@ import org.bukkit.DyeColor;
  * Represents a color option for a team.
  */
 public enum TeamColor {
+    DARK_RED(ChatColor.DARK_RED, Color.MAROON, DyeColor.RED),
     RED(ChatColor.RED, Color.RED, DyeColor.RED),
     ORANGE(ChatColor.GOLD, Color.ORANGE, DyeColor.ORANGE),
     YELLOW(ChatColor.YELLOW, Color.YELLOW, DyeColor.YELLOW),
@@ -15,10 +16,13 @@ public enum TeamColor {
     DARK_GREEN(ChatColor.DARK_GREEN, Color.GREEN, DyeColor.GREEN),
     AQUA(ChatColor.AQUA, Color.AQUA, DyeColor.LIGHT_BLUE),
     BLUE(ChatColor.BLUE, Color.BLUE, DyeColor.BLUE),
+    DARK_BLUE(ChatColor.DARK_BLUE, Color.NAVY, DyeColor.BLUE),
     PURPLE(ChatColor.DARK_PURPLE, Color.PURPLE, DyeColor.PURPLE),
     PINK(ChatColor.LIGHT_PURPLE, Color.FUCHSIA, DyeColor.PINK),
     BLACK(ChatColor.BLACK, Color.BLACK, DyeColor.BLACK),
-    WHITE(ChatColor.WHITE, Color.WHITE, DyeColor.WHITE);
+    WHITE(ChatColor.WHITE, Color.WHITE, DyeColor.WHITE),
+    GRAY(ChatColor.GRAY, Color.SILVER, DyeColor.SILVER),
+    DARK_GRAY(ChatColor.DARK_GRAY, Color.GRAY, DyeColor.GRAY);
 
     private final ChatColor chatColor;
     private final Color leatherColor;
